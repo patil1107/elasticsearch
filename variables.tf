03015d5503b25a3fb5f45e8fe47d2aa8dd336e1c
patil1107 <patil1107@gmail.com>
2020-04-16 18:06:53 +0530

    Updated Variable file

--- variables.tf
+++ variables.tf
@@ -3,19 +3,11 @@ variable "aws_region" {
 }
 
 variable "aws_secret_key" {
-<<<<<<< HEAD
  default = "geIr+w5FXFJueYYXvfyPWfVYex42GWZLzvvN8ntz"
 }
 
 variable "aws_access_key" {
  default = "AKIAVZL6VGUY7XRGBMXD"
-=======
- default = ""
-}
-
-variable "aws_access_key" {
- default = ""
->>>>>>> ce5f75ec6c5069062c1fe3c4b543385c5479f56e
 }
 
 variable "aws_ami" {
